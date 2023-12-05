8efaa3e8e15bf1acda5e4f8170f952633d006ba7
Petrik Diak <diak@petrik.hu>
2023-12-05 08:41:56 +0100

    asd

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+import express from 'express';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+
+const app = express();
+
+app.use(cors());
+app.listen()
