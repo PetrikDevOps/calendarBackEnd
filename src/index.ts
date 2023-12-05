6b269e01d72afdbe258d782dde59995813b8d48b
Petrik Diak <diak@petrik.hu>
2023-12-05 08:45:27 +0100

    404

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
@@ -8,4 +9,12 @@ import bcrypt from 'bcrypt';
 const app = express();
 
 app.use(cors());
-app.listen()
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.use((req, res) => {
+    return res.status(404).json({Error: "Missing Page (404)"});
+})
+
+app.listen(process.env.PORT || 3000);
