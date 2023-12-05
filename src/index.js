a2b0d117eb11300a6810b2707a51e48ed754d7d4
hrustinszkiadam <hrustinszkiadam@gmail.com>
2023-12-05 13:22:22 +0100

    login and getting user, added token

--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import validator from 'validator';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
 import { connectToDB, q as query } from './db/db';
 import { UserService } from './services/userService';
 
 const app = express();
 const userService = new UserService();
-const { checkUser, validateEmail, checkUsername } = userService;
+const { checkUser, validateEmail, checkUsername, verifyUser } = userService;
 
 app.use(cors({}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
 const users = await connectToDB();
+
+
+app.get('/', verifyUser, (req, res) => {
+    const sql = `SELECT * FROM users WHERE id = ${req.id}`;
+    try {
+        query(users, sql);
+    }
+    catch(err) {
+        return res.status(400).json({Error: 'Error getting user'});
+    }
+    return res.status(200).json({Success: 'Logged in', user: {id: result[0].id, username: result[0].username, email: result[0].email}});
+  });
 
 app.post('/register', validateEmail, checkUser, checkUsername, (req, res) => {
     if(req.body.status.Error) {
@@ -31,7 +45,29 @@ app.post('/register', validateEmail, checkUser, checkUsername, (req, res) => {
         }
         return res.status(200).json({Success: 'User successfully created'})
     }) 
-})
+});
+
+app.post('/login', (req, res) => {
+    const sql = `SELECT * FROM users WHERE email = ${req.body.email}`;
+    let result = [];
+    try {
+        result = query(users, sql);
+    } catch (err) {
+        return res.status(400).json({Error: 'Error logging in'});
+    };
+    if(result.length === 0) return res.status(400).json({Error: 'No user found'});
+    bcrypt.compare(req.body.password.toString(), result[0].password, (err, response) => {
+        if(err) return res.status(400).json({Error: 'Error logging in'});
+        if(response) {
+            const id = result[0].id;
+            const token = jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: process.env.TOKEN_EXPIRE_TIME || '1w'});
+            res.cookie('token', token);
+            return res.status(200).json({Success: 'Logged in'});
+        } else {
+            return res.status(400).json({Error: 'Incorrect password'});
+        }
+    })
+});
 
 app.use((req, res) => {
     return res.status(404).json({Error: "Missing Page (404)"});
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,9 @@
+import validator from 'validator';
+import { connectToDB, q as query } from './db/db';
+import jwt from 'jsonwebtoken';
+
+const users = await connectToDB();
+
 export class UserService {
     checkUser = (req, res, next) => {
         const sql = `SELECT * FROM users WHERE email = ${req.body.email}`;
@@ -6,7 +12,7 @@ export class UserService {
     };
 
     validateEmail = (req, res, next) => {
-        if(!req.body.email == validator.isEmail()) return res.status(400).json({Error: 'Invalid Email'})
+        if(!req.body.email || !validator.isEmail(req.body.email)) return res.status(400).json({Error: 'Invalid Email'});
         next();
     };
 
@@ -15,4 +21,16 @@ export class UserService {
         if(query(users, sql).length > 0) return res.status(400).json({Error: 'Username taken'})
         next();
     }
+
+    verifyUser = (req, res, next) => {
+        const token = req.cookies.token;
+        if (!token) return res.json({Error: 'No token found'});
+      
+        jwt.verify(token, process.env.JWT_SECRET, (err, result) => {
+          if (err) return res.json({Error: 'Invalid token'});
+      
+          req.id = result.id;
+          next();
+        });
+      }
 }
