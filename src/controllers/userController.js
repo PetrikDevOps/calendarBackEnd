ce3335b953e353a97e3f93634374d4a1b3076606
hrustinszkiadam <hrustinszkiadam@gmail.com>
2023-12-09 14:18:17 +0100

    a

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,10 +2,8 @@ import UserService from '../services/userService.js';
 import dbService from '../services/dbService.js';
 import CalendarService from '../services/calendarService.js';
 import express from 'express';
-import cookieParser from 'cookie-parser';
 
 const userController = express.Router();
-userController.use(cookieParser());
 const db = new dbService();
 const calendar = new CalendarService(db);
 const userService = new UserService(db, calendar);
